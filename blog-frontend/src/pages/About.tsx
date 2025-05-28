15ad2074da2d8a4b784b4d80246ad400a8d3f650
Youssef-Malek2004 <161763492+Youssef-Malek2004@users.noreply.github.com>
2025-05-29 01:53:33 +0300

    Wording in About

--- blog-frontend/src/pages/About.tsx
+++ blog-frontend/src/pages/About.tsx
@@ -44,7 +44,7 @@ const About = () => {
         />
 
         <Text mt={4} fontSize="md" color={secondary}>
-          Hi there! I am <strong>Youssef Malek</strong>, a senior computer engineering student at{" "}
+          Hi there! I am <strong>Youssef Malek</strong>, a junior computer engineering student at{" "}
           <Link
             href="https://www.guc.edu.eg/"
             display="inline-flex"
